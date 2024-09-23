ab34a8998911da47cda7ee5d800a677a61df9c8c
dylanR96 <dylan.ravenson@gmail.com>
2024-09-23 20:07:10 +0200

    Added functions for api calls, created db file

---
+++ dynamoDb.ts
@@ -0,0 +1,10 @@
+import { DynamoDB} from "@aws-sdk/client-dynamodb"
+import { DynamoDBDocument} from "@aws-sdk/lib-dynamodb"
+
+const client = new DynamoDB({
+  region: "eu-north-1",
+})
+
+const db = DynamoDBDocument.from(client)
+
+export {db}
---
+++ functions/getMessages.ts
@@ -0,0 +1,25 @@
+import {db} from "../dynamoDb"
+import { ScanCommand } from "@aws-sdk/lib-dynamodb"
+import { APIGatewayProxyResult } from 'aws-lambda';
+
+const handler = async (): Promise<APIGatewayProxyResult> => {
+  try {
+    const result = await db.send(new ScanCommand({
+      TableName: "MessageBoard",
+    }))
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({ message: result.Items})
+    }
+    return response
+  } catch (error) {
+    console.error(error)
+    return{
+      statusCode: 500,
+      body: JSON.stringify({ message: "Error while scanning the table", error: (error as Error).message})
+    }
+  }
+}
+
+export default handler;
